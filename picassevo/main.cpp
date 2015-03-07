a24e62eb4e0292509a58146927824b1ab569c0c1
Misha Denil <mdenil@google.com>
2015-03-07 18:00:27 +0000

    Squares.png

--- picassevo/main.cpp
+++ picassevo/main.cpp
@@ -51,7 +51,8 @@ int main(int argc, char** argv)
     //FitnessFunction fitness_function("data/lisa.png");
     //FitnessFunction fitness_function("data/starry-night.png");
     //FitnessFunction fitness_function("data/bridge.png");
-    FitnessFunction fitness_function("data/explosion.png");
+    //FitnessFunction fitness_function("data/explosion.png");
+    FitnessFunction fitness_function("data/squares.png");
     
     image_width = fitness_function.get_width();
     image_height = fitness_function.get_height();
